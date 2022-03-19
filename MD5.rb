9441cb41ca6945020f00da0af70c3dce2db75c69
Kuper <killkih@mail.ru>
2022-03-19 13:25:27 +0300

    version 1

---
+++ MD5.rb
@@ -0,0 +1,10 @@
+#UTF-8
+require "digest"
+
+puts "Ведите текст, который хотите зашифровать:"
+words = STDIN.gets.chomp
+
+encry_words = Digest::MD5.hexdigest(words)
+
+puts "Ваш текст был зашифрован в: #{encry_words}"
+
